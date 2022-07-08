4df1194d6a7cf7d6c0df66ad3417fe54880d69f9
Ashton Gabrielsen <ashton.gabrielsen@drip.com>
2022-07-08 14:38:00 -0600

    mock playlist data

--- api/routes/playlist.js
+++ api/routes/playlist.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const data = require('../playlistData.json');
+
 router.get('/', function(req, res, next) {
-  res.send('API');
+  res.json(data);
 });
 
 module.exports = router;
