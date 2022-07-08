a3a520cd3236a84a7d7c79b8342cd1608bdd865e
Ashton Gabrielsen <ashton.gabrielsen@drip.com>
2022-07-08 15:41:48 -0600

    remove data damp

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import Header from './components/Header.js';
 import Playlist from './components/Playlist.js';
 import Album from './components/Album.js';
 
-
 function App() {
+  const [playlist, setPlaylist] = useState('');
+
+  useEffect(() => {
+    fetch(
+      `http://localhost:9000/playlist`,
+      {
+        method: "GET"
+      }
+    )
+      .then(res => res.text())
+      .then(res => {
+        setPlaylist(res);
+      })
+      .catch(err => err);
+  });
+
   return (
     <div className={"row flex-row-reverse gx-0"}>
       <div className={"col-md-5"}>
