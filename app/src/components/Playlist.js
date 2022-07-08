2ce77132560f0011f6be85d590476416f6f37512
Ashton Gabrielsen <ashton.gabrielsen@drip.com>
2022-07-08 11:57:13 -0600

    up next

--- app/src/components/Playlist.js
+++ app/src/components/Playlist.js
@@ -1,6 +1,9 @@
 function Playlist() {
   return (
     <div className={"bg-secondary"}>
+      <div>
+        <h5 className={"mb-0 py-2 align-self-center"}>Up Next</h5>
+      </div>
       <ul className="list-group list-group-horizontal bg-white border-0 rounded-0">
         <li className="col-1 list-group-item rounded-0 d-flex justify-content-center align-items-center border-0">1</li>
         <li className="col-8 list-group-item rounded-0 align-self-center border-0">
