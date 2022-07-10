b827282c9a45b2af86ab837a6d8dfafd23e8acb4
Ashton Gabrielsen <ashton.gabrielsen@drip.com>
2022-07-10 15:59:34 -0600

    iterate over playlist

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import Header from './components/Header.js';
@@ -6,21 +6,46 @@ import Playlist from './components/Playlist.js';
 import Album from './components/Album.js';
 
 function App() {
-  const [playlist, setPlaylist] = useState('');
+  const [playlist, setPlaylist] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    fetch(
-      `http://localhost:9000/playlist`,
-      {
-        method: "GET"
+    const fetchPlaylist = async () => {
+      setIsLoading(true);
+
+      console.log(isLoading);
+      try {
+        const res = await fetch(
+          'http://localhost:9000/playlist',
+        ).then(res => res.text())
+          .then(res => {
+            setPlaylist(JSON.parse(res));
+          });
+      } catch (error) {
+
       }
-    )
-      .then(res => res.text())
-      .then(res => {
-        setPlaylist(res);
-      })
-      .catch(err => err);
-  });
+      setIsLoading(false);
+    };
+    fetchPlaylist();
+  }, [setPlaylist]);
+
+  // const fetchPlaylist = () => {
+  //   fetch(
+  //     `http://localhost:9000/playlist`,
+  //     {
+  //       method: "GET"
+  //     }
+  //   )
+  //     .then(res => res.text())
+  //     .then(res => {
+  //       setPlaylist(JSON.parse(res));
+  //     })
+  //     .catch(err => err)
+  // };
+
+  // useEffect(() => {
+  //   fetchPlaylist();
+  // }, []);
 
   return (
     <div className={"row flex-row-reverse gx-0"}>
@@ -33,7 +58,7 @@ function App() {
           <Header />
         </div>
 
-        <Playlist />
+        { isLoading ? <p>Loading...</p> : <Playlist playlist={playlist.items} /> }
       </div>
     </div>
   );
